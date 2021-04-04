d0d0fa290af04d27dee3815276301e4b5d9ab355
Naeem <naeem3d@gmail.com>
2021-04-03 22:47:32 -0400

    Initial Commit

---
+++ xylophone2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  xylophone2
+//
+//  Created by naeem alabboodi on 4/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
